31a63c69cc71fadd46478ff82e7ffc03b7845c0e
Sravya <boinapally@Sravya>
2019-11-05 13:36:28 +0530

    JDBC case study

--- SpringJdbcCaeStudy/src/main/java/com/sravya/SpringJdbcCaeStudy/PersonDAO.java
+++ SpringJdbcCaeStudy/src/main/java/com/sravya/SpringJdbcCaeStudy/PersonDAO.java
@@ -2,27 +2,11 @@ package com.sravya.SpringJdbcCaeStudy;
 
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-
-public class PersonDAO {
-private DataSource dataSource;
-public void setDataSource(DataSource dataSource) {
-this.dataSource=dataSource;
-}
-public void insert(String name,String email) {
-	JdbcTemplate jdbcTemplate=new JdbcTemplate(dataSource);
-	jdbcTemplate.update("insert into casestudy.person(name,email) values(?,?)",new Object[] {name,email});
-}
-public List<Person> selectALL(){
-	JdbcTemplate jdbcTemplate=new JdbcTemplate(dataSource);
-	String sql="SELECT * FROM casestudy.person";
-    return jdbcTemplate.query(sql,new PersonRowMapper());
-}
-public void deletePerson(String name) {
-	JdbcTemplate jdbcTemplate=new JdbcTemplate(dataSource);
-	String sql="delete from casestudy.person where name=?";
-	jdbcTemplate.update(sql,new Object[] {name});
-}
+public interface PersonDAO {
+	
+	public void insert(String name,String email);
+	
+	public List<Person> selectALL();
+	
+	public void deletePerson(String name);
 }
